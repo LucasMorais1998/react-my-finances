f859251ba222dbfa95b60be046fb7891ffb8ba70
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-13 13:57:53 -0300

    add global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,44 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --bg-color: darkblue;
+
+    --white-color: #fff;
+    --blue-color: blue;
+    --grey-color: #888;
+  }
   
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background-color: var(--bg-color);
+    -webkit-font-smoothing: antialiaased;
+  }
+
+  body, input, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  } 
+
+  button {
+    cursor: pointer;
+  }
 `;
