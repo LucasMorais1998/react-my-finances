cf97de1c13c580e674019bd45e8237a4df25d7a4
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-13 20:38:32 -0300

    add Dashboard features

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+
+import { Item } from "../../@types/Item";
+import { Category } from "../../@types/Category";
+import { items } from "../../data/items";
+import { categories } from "../../data/categories";
+
+import { getCurrentMonth, filterListByMonth } from "../../helpers/dateFilter";
+
 import { Container } from "./styles";
 
 const Dashboard = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+
+  },[list, currentMonth])
+
+
   return (
     <Container>
       <h1>Dashboard</h1>
