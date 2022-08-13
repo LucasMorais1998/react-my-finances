a1e293b28c9ab9947aae7df4d07e078cca5c90ef
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-13 14:39:37 -0300

    add Item data

---
+++ src/data/items.ts
@@ -0,0 +1,27 @@
+export const items = [
+  {
+    date: new Date(2022, 7, 6),
+    category: "supermarket",
+    title: "Supermercado",
+    value: 750.12,
+  },
+  {
+    date: new Date(2022, 7, 13),
+    category: "food",
+    title: "McDonalds",
+    value: 48.64,
+  },
+
+  {
+    date: new Date(2022, 7, 15),
+    category: "rent",
+    title: "Aluguel",
+    value: 1200,
+  },
+  {
+    date: new Date(2022, 7, 31),
+    category: "salary",
+    title: "Salário",
+    value: 3500,
+  },
+];
