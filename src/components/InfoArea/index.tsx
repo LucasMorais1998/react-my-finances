89b7dcfe0cb3a24a7c8de1734569b4c63cd0ebd9
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-14 17:59:44 -0300

    add InfoArea style

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,14 +1,17 @@
-import { Container, MonthArea, ResumeArea } from "./styles";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+import { Container, MonthArea, ResumeArea, MonthTitle } from "./styles";
 
 const InfoArea = () => {
   return (
     <Container>
       <MonthArea>
-
+        <FaArrowLeft className="arrow-left-icon" />
+        <MonthTitle>Title</MonthTitle>
+        <FaArrowRight className="arrow-right-icon" />
       </MonthArea>
-      <ResumeArea>
-        
-      </ResumeArea>
+
+      <ResumeArea></ResumeArea>
     </Container>
   );
 };
--- src/components/InfoArea/styles.ts
+++ src/components/InfoArea/styles.ts
@@ -6,8 +6,34 @@ export const Container = styled.div`
   border-radius: 0.625rem;
   padding: 1.25rem;
   margin-top: -6.5rem;
+  display: flex;
+  align-items: center;
 `;
 
-export const MonthArea = styled.div``
+export const MonthArea = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 
-export const ResumeArea = styled.div``;
+  .arrow-left-icon,
+  .arrow-right-icon {
+    font-size: 1.5rem;
+    color: #111;
+    cursor: pointer;
+    transition: all ease 0.3s;
+
+    &:hover {
+      color: darkblue;
+    }
+  }
+`
+
+export const MonthTitle = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+export const ResumeArea = styled.div`
+  flex: 2;
+  display: flex;
+`;
