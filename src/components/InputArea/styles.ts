80b42f37c90b8b260203664636733843eef8da55
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-17 18:15:56 -0300

    add InputArea style

--- src/components/InputArea/styles.ts
+++ src/components/InputArea/styles.ts
@@ -6,4 +6,71 @@ export const Container = styled.div`
   border-radius: 0.625rem;
   padding: 1.25rem;
   margin-top: 1.25rem;
+
+  form {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 0.55rem;
+
+    label {
+      font-weight: 600;
+
+      input,
+      select,
+      option {
+        margin-left: 0.3rem;
+        padding: 0.2rem;
+        font-size: 0.9rem;
+        font-weight: 500;
+      }
+
+      input,
+      select {
+        border: none;
+        border: 1px solid #000;
+        border-radius: 0.3rem;
+      }
+
+      input[id="date"] {
+        width: 7.5rem;
+      }
+
+      input[id="title"] {
+        width: 12rem;
+      }
+
+      input[id="value"] {
+        width: 6.5rem;
+      }
+
+      option {
+        font-weight: 500;
+      }
+
+      option[disabled] {
+        font-weight: 400;
+        color: rgba(0, 0, 0, 0.1);
+      }
+    }
+
+    input[type="submit"] {
+      width: 5rem;
+      height: 1.75rem;
+      border: none;
+      border-radius: 0.3rem;
+      font-weight: bold;
+      color: var(--white-color);
+      background-color: var(--darkblue-color);
+      cursor: pointer;
+      opacity: 0.8;
+      transition: opacity ease 0.2s;
+    }
+
+    input[type="submit"]:hover {
+      opacity: 1;
+      
+    }
+  }
 `;
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -4,6 +4,6 @@ export const categories: Category = {
   income: { title: "Renda Extra", color: "#33CC95", expense: false },
   food: { title: "Alimentação", color: "#6933FF", expense: true },
   rent: { title: "Aluguel", color: "#E62E4D", expense: true },
-  salary: { title: "Salário", color: "#00cccc", expense: false },
-  market: { title: "Mercado", color: "#f7d354", expense: true },
+  salary: { title: "Salário", color: "#00CCCC", expense: false },
+  market: { title: "Mercado", color: "#F7D354", expense: true },
 };
