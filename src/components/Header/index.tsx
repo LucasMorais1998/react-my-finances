e937b5bd78120bdabd842eeb2234d4fa561f9324
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-19 17:40:06 -0300

    add icon in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
+import { BsCashCoin } from "react-icons/bs";
+
 import { Container } from "./styles";
 
 const Header = () => {
   return (
     <Container>
       <h1>my Finances</h1>
+      <BsCashCoin className="header-title-icon"/>
     </Container>
   );
 };
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -4,9 +4,20 @@ export const Container = styled.header`
   height: 30vh;
   background-color: var(--darkblue-color);
   text-align: center;
+  padding-bottom: 4rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
 
   h1 {
     color: var(--white-color);
-    padding-top: 1.875rem;
+  }
+
+  .header-title-icon {
+    color: var(--white-color);
+    font-size: 1.5rem;
+    margin-top: 0.7rem;
   }
 `;
