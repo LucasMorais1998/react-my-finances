cead11a130e2ac0aa940ebd58283bbe6442dc58e
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-08 22:16:19 -0300

    fix data/item

--- src/data/items.ts
+++ src/data/items.ts
@@ -2,49 +2,49 @@ import { Item } from "../@types/Item";
 
 export const items: Item[] = [
   {
-    date: new Date(2022, 6, 15),
-    category: "rent",
-    title: "Aluguel",
-    value: 1200,
-  },
-  {
-    date: new Date(2022, 6, 20),
-    category: "market",
-    title: "Supermercado",
-    value: 873,
-  },
-  {
-    date: new Date(2022, 6, 31),
-    category: "salary",
-    title: "Salário",
-    value: 3500,
-  },
-  {
-    date: new Date(2022, 7, 6),
-    category: "income",
-    title: "Freelance de website",
-    value: 1750,
-  },
-  {
-    date: new Date(2022, 7, 13),
-    category: "food",
-    title: "McDonalds",
-    value: 48.64,
-  },
-  {
     date: new Date(2022, 7, 15),
     category: "rent",
     title: "Aluguel",
     value: 1200,
   },
   {
-    date: new Date(2022, 7, 17),
+    date: new Date(2022, 7, 20),
+    category: "market",
+    title: "Supermercado",
+    value: 873,
+  },
+  {
+    date: new Date(2022, 7, 31),
+    category: "salary",
+    title: "Salário",
+    value: 3500,
+  },
+  {
+    date: new Date(2022, 8, 6),
+    category: "income",
+    title: "Freelance de website",
+    value: 1750,
+  },
+  {
+    date: new Date(2022, 8, 13),
+    category: "food",
+    title: "McDonalds",
+    value: 48.64,
+  },
+  {
+    date: new Date(2022, 8, 15),
+    category: "rent",
+    title: "Aluguel",
+    value: 1200,
+  },
+  {
+    date: new Date(2022, 8, 17),
     category: "market",
     title: "Mercado",
     value: 873.25,
   },
   {
-    date: new Date(2022, 7, 31),
+    date: new Date(2022, 8, 31),
     category: "salary",
     title: "Salário",
     value: 3500,
